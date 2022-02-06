69106684a80c0c6f930cdc8c47191c141b5f75c8
LamkadamNabil <na.lamkadam@gmail.com>
2022-02-06 11:51:44 +0100

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import GradientLayout from './components/gradientLayout'
 
 export default function Home() {
   return (
-  <div>HOME</div>
+    <GradientLayout color="red">
+           <div>
+             hello
+           </div>
+    </GradientLayout>
   )
 }
