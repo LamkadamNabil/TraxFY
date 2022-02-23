05de1908d8fd40d5adeca2506749c207bb1a9195
LamkadamNabil <na.lamkadam@gmail.com>
2022-02-24 00:35:24 +0100

    add songs table and playlist done

---
+++ pages/playlist/[id].tsx
@@ -0,0 +1,59 @@
+import { validateToken } from "../../lib/auth"
+import prisma from "../../lib/prisma"
+import GradientLayout from "../components/gradientLayout"
+import SongTable from "../components/songsTable"
+const getBG =id =>{
+    const color =[
+        'red',
+        'green',
+        'blue',
+        'orange',
+        'purple',
+        'gray',
+        'teal',
+        'yellow',
+      ]
+    
+    return color[id -1] || color[Math.floor(Math.random()*color.length)]
+}
+const Playlist=({playlist})=> {
+    const  color =getBG(playlist.id) ;
+  return (
+  <GradientLayout 
+    color={color}
+    roundImage={false}
+    title={playlist.name}
+    subtitle="playlist"
+    description={`${playlist.songs.length} songs`}
+    image={`https://picsum.photos/400?random=${playlist.id}`}
+  >
+<SongTable songs={playlist.songs} />
+  </GradientLayout>
+    )
+}
+export const getServerSideProps =async({query ,req })=>{
+  const {id} =validateToken(req.cookies.NABIL_SPOTIFY_ACCESS_TOKEN)  ;
+  const [playlist ] =await prisma.playlist.findMany({
+      where :{
+          id :+query.id ,
+          userId :id ,
+      },
+      include :{
+          songs :{
+              include :{
+                  artist :{
+                      select :{
+                          name :true ,
+                          id :true ,
+                      }
+                  }
+              }
+          }
+      }
+  })
+  return {
+      props :{playlist},
+  }
+
+}
+export default Playlist
